38950abab722e211deb11fcb9cd68a6698538d34
SahilMakvana <67103415+SahilMakvana@users.noreply.github.com>
2024-04-04 00:00:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,4 +24,4 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
 
 mongoose.set('useFindAndModify', false);
 
-// Developed By SahilMakva
+// Developed By SahilMakv
